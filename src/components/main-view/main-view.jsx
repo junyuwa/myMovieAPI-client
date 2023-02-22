bc55804423b17a001f98ad853434c6b1bb566822
junyuwa <wjyselina@hotmail.com>
2023-02-21 21:05:48 -0800

    connect to movie api, check w prop types

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,35 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Cloud Atlas",
-            image: "https://m.media-amazon.com/images/M/MV5BMTczMTgxMjc4NF5BMl5BanBnXkFtZTcwNjM5MTA2OA@@._V1_FMjpg_UX1000_.jpg",
-            description: "An exploration of how the actions of individual lives impact one another in the past, present and future, as one soul is shaped from a killer into a hero, and an act of kindness ripples across centuries to inspire a revolution.",
-            genre: "sci-fi",
-            director: "Tom Tykwer"
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        fetch("https://wjy-movies-api.herokuapp.com/movies")
+            .then((res) => res.json())
+            .then((data) => {
+                const movieResults = data.movies.map((movie) => {
+                    return {
+                        title: movie.title,
+                        description: movie.description,
+                        genre: movie.genre.name,
+                        director: movie.director.name
+                    }
+                })
 
-        },
-        {
-            id: 2,
-            title: "Rocky Horror Picture Show",
-            image: "https://m.media-amazon.com/images/M/MV5BOGIzYjM3YzMtMjk5ZS00NDY2LTllMjEtNjYwZjhmMDNhMDBkXkEyXkFqcGdeQXVyODUzMjQxMTA@._V1_.jpg",
-            description: "A newly-engaged couple have a breakdown in an isolated area and must seek shelter at the bizarre residence of Dr. Frank-n-Furter.",
-            genre: "horror",
-            director: "Jim Sharman"
-        },
-        {
-            id: 3,
-            title: "Lust Caution",
-            image: "https://m.media-amazon.com/images/M/MV5BMTg2MGJjYmQtY2RjNy00NjczLWJlNDYtOWIzOGE2NGRjN2RhXkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_.jpg",
-            description: "During World War II era, a young woman, Wang Jiazhi, gets swept up in a dangerous game of emotional intrigue with a powerful political figure, Mr. Yee.",
-            genre: "romance",
-            director: "Ang Lee"
-        }
-    ]);
+                setMovies(movieResults);
+            })
+    }, []);
 
     const [selectedMovie, setSelectedMovie] = useState(null)
 
@@ -44,7 +34,7 @@ export const MainView = () => {
     return (
         <div>
             {movies.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} onMovieClick={(newSelectedMovie) => {
+                <MovieCard key={movie.title} movie={movie} onMovieClick={(newSelectedMovie) => {
                     setSelectedMovie(newSelectedMovie);
                 }} />
             )
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
@@ -5,7 +7,22 @@ export const MovieCard = ({ movie, onMovieClick }) => {
                 onMovieClick(movie);
             }}
         >
-            {movie.title}
+            {movie.Title}
         </div>
     );
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        ImagePath: PropTypes.string.isRequired,
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+        }),
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 };
